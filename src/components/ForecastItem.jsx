f0ecbbb1c8998c197768d90254b41b3783e80779
enyolka <maczka.emilia@gmail.com>
2021-03-22 12:36:57 +0100

    feature: informations about forecast weather

--- src/components/ForecastItem.jsx
+++ src/components/ForecastItem.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const ForecastItem = ({ data }) => {
-  return <li className="forecast__item">{data.dt_txt}</li>;
+  const temperature = Number((data.main.temp - 273).toFixed(0));
+
+  const icon = (
+    <img
+      className="icon"
+      src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+      alt={data.weather[0].main}
+    />
+  );
+
+  return (
+    <li className="forecast__item">
+      <span>{data.dt_txt.split(" ")[0]}</span>
+      <div>{icon}</div>
+      <div className="forecast__temperature">{`${temperature} \xB0C`}</div>
+      <div className="forecast__description">{data.weather[0].description}</div>
+    </li>
+  );
 };
 
 export default ForecastItem;
